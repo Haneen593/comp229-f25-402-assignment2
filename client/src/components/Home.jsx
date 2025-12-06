7e8fc5d567b6fe2b51a52a291be37e98cc5cee0c
Haneen Ftayeh <hftayeh@my.centennialcollge.ca>
2025-12-06 18:32:02 -0500

    Changed the title in home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <div className="message">
-                <h1>Welcome to My Portfolio</h1>
+                <h1>Hello and Welcome!</h1>
                 <p>My mission is to grow as a software engineer 
                 by committing to continuous learning and enhancing my skills in AI and software development, 
                 so I can contribute to meaningful projects that improve everyday life.
